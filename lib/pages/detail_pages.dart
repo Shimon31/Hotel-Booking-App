b8655ce1e1ff22ef2fe596871b93856524c8148f
shimon <shimonbhuiya02@gmail.com>
2026-04-20 02:00:54 +0600

    Add detail_pages.dart

---
+++ lib/pages/detail_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailPages extends StatefulWidget {
+  const DetailPages({super.key});
+
+  @override
+  State<DetailPages> createState() => _DetailPagesState();
+}
+
+class _DetailPagesState extends State<DetailPages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
